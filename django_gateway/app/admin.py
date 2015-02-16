52f25766943cc014929b03422ce04aa69c932b06
Martin Freytes <martin.jfreytes@gmail.com>
2015-02-16 11:52:04 -0500

    Merchant Transaction at administrator site

--- django_gateway/app/admin.py
+++ django_gateway/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app.models import *
 
-# Register your models here.
+
+@admin.register(MerchantTransaction)
+class MerchantTransactionAdmin(admin.ModelAdmin):
+    list_display = ('merchant_id', 'user', 'gateway', 'status')
+    list_filter = ('merchant_id', 'user__username', 'gateway', 'status')
+    search_fields = ('merchant_id', 'user__username', 'gateway', 'status')
